34c863fcead711687f4295641a728cf9f90b34ac
Samproltd <testingsampro@gmail.com>
2025-03-04 18:03:36 +0530

    Resolved merge conflicts and added changes

--- src/main/java/com/exam/config/JwtAuthenticationFilter.java
+++ src/main/java/com/exam/config/JwtAuthenticationFilter.java
@@ -2,13 +2,12 @@ package com.exam.config;
 
 import com.exam.service.impl.UserDetailsServiceImpl;
 import io.jsonwebtoken.ExpiredJwtException;
-import org.apache.catalina.User;
-import org.apache.catalina.security.SecurityConfig;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
