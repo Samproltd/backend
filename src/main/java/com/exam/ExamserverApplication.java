e77584bc2dc12d2b520b873e172b2db5688d81eb
Samproltd <testingsampro@gmail.com>
2025-03-18 14:32:00 +0530

    Using Azure SQL Database

--- src/main/java/com/exam/ExamserverApplication.java
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -1,20 +1,34 @@
 package com.exam;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.exam.helper.UserFoundException;
+import com.exam.model.Role;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.model.UserType;
+import com.exam.repo.QuizRepository;
+import com.exam.service.UserService;
 
 @SpringBootApplication
-public class ExamserverApplication{
-//	 implements CommandLineRunner{ 
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-//    @Autowired
-//    public QuizRepository quizRepository;
+public class ExamserverApplication
+	 implements CommandLineRunner{ 
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public QuizRepository quizRepository;
 
     public static void main(String[] args) {
 
@@ -24,48 +38,48 @@ public class ExamserverApplication{
 
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//        try {
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+
+
+            System.out.println("starting code");
 //
-//
-//            System.out.println("starting code");
-////
-//            User user = new User();
-//
-//            user.setFirstName("Bhushan");
-//            user.setLastName("Galphade");
-//            user.setUsername("bhushan@gmail.com");
-//            user.setPassword(this.bCryptPasswordEncoder.encode("bhushan123"));
-//            user.setEmail("bhushan@gmail.com");
-////            user.setProfile("default.png");
-//            user.setPhone("9359339299");
-//           user.setUserType(UserType.GENERAL);
-//            Role role1 = new Role();
-//            role1.setRoleId(44L);
-//            role1.setRoleName("ADMIN");
-//
-//            Set<UserRole> userRoleSet = new HashSet<>();
-//            UserRole userRole = new UserRole();
-//
-//            userRole.setRole(role1);
-//
-//            userRole.setUser(user);
-//
-//            userRoleSet.add(userRole);
-//
-//            User user1 = this.userService.createUser(user, userRoleSet);
-//            System.out.println(user1.getUsername());
-//
-//
-//        } catch (UserFoundException e) {
-//            e.printStackTrace();
-//
-//
-//        }
-//
-//
-//    }
-//
+            User user = new User();
+
+            user.setFirstName("Bhushan");
+            user.setLastName("Galphade");
+            user.setUsername("bhushan@gmail.com");
+            user.setPassword(this.bCryptPasswordEncoder.encode("bhushan123"));
+            user.setEmail("bhushan@gmail.com");
+//            user.setProfile("default.png");
+            user.setPhone("9359339299");
+           user.setUserType(UserType.GENERAL);
+            Role role1 = new Role();
+            role1.setRoleId(44L);
+            role1.setRoleName("ADMIN");
+
+            Set<UserRole> userRoleSet = new HashSet<>();
+            UserRole userRole = new UserRole();
+
+            userRole.setRole(role1);
+
+            userRole.setUser(user);
+
+            userRoleSet.add(userRole);
+
+            User user1 = this.userService.createUser(user, userRoleSet);
+            System.out.println(user1.getUsername());
+
+
+        } catch (UserFoundException e) {
+            e.printStackTrace();
+
+
+        }
+
+
+    }
+
 
 }
